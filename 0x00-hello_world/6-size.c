fee2ada2e37410d14dc051bd01a5bc7381d453fe
Smile2-beep <bammidelealli@gmail.com>
2023-03-18 21:40:11 -0700

    my project

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+  /**
+  * main - Entry point of the program
+  * 
+  * Description: using the main function
+  * Return: Always 0 (Success)
+  */
+  int main(void)
+  {
+    char c;
+    int i;
+    long li;
+    long long lli;
+    float f:
+    
+    print("size of a char: %ld byte(s)\n", size of(char));
+    print("size of a int: %ld byte(s)\n", size of(int));
+    print("size of a long: %ld byte(s)\n", size of(long));
+    print("size of a long long: %ld byte(s)\n", size of(long long));
+    print("size of a float: %ld byte(s)\n", size of(float));
+    Return (0);
+  }
