55a611fd0cfd941379a97e3c87d31253598408ea
Smile2-beep <bammidelealli@gmail.com>
2023-03-18 23:14:52 -0700

    my second

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  
 /**
-* main - print the string in the put function
+* main  print the string in the put function
 * 
 * Description: using the main function
 * this program prints "programming is like building a multilingual puzzle
@@ -14,12 +14,12 @@ int main(void)
         long li;
         long li;
         long long lli;
-        float f:
+        float f;
     
-        print("size of a char: %ld byte(s)\n", size of(c));
-        print("size of a int: %ld byte(s)\n", size of(i));
-        print("size of a long: %ld byte(s)\n", size of(li));
-        print("size of a long long: %ld byte(s)\n", size of(lli));
-        print("size of a float: %ld byte(s)\n", size of(float));
+        print("size of a char: %ld byte(s)\n", sizeof(c));
+        print("size of a int: %ld byte(s)\n", sizeof(i));
+        print("size of a long: %ld byte(s)\n", sizeof(li));
+        print("size of a long long: %ld byte(s)\n", sizeof(lli));
+        print("size of a float: %ld byte(s)\n", sizeof(f));
         return (0);
 }
