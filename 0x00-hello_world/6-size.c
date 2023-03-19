1822fa0b0e6eaf2b4639a16b7ba26cad99a616ea
Smile2-beep <bammidelealli@gmail.com>
2023-03-18 22:38:15 -0700

    my third commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,20 +4,21 @@
 * main - Entry point of the program
 * 
 * Description: using the main function
-* Return: Always 0 (Success)
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
-    char c;
-    int i;
-    long li;
-    long long lli;
-    float f:
-    
-    print("size of a char: %ld byte(s)\n", size of(char));
-    print("size of a int: %ld byte(s)\n", size of(int));
-    print("size of a long: %ld byte(s)\n", size of(long));
-    print("size of a long long: %ld byte(s)\n", size of(long long));
-    print("size of a float: %ld byte(s)\n", size of(float));
-    return (0);
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f:
+
+	print("size of a char: %ld byte(s)\n", size of(char));
+        print("size of a int: %ld byte(s)\n", size of(int));
+        print("size of a long: %ld byte(s)\n", size of(long));
+        print("size of a long long: %ld byte(s)\n", size of(long long));
+        print("size of a float: %ld byte(s)\n", size of(float));	
 }
+
